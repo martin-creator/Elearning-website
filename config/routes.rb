5c41bb97cba385bd9a0399e727ceb871a45d49b7
martin-creator <martinlubowa@outlook.com>
2022-08-13 12:28:28 +0300

    Added avatar picture to login page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   get 'pages/about'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root 'pages#about'  #This becomes the landing page for our website
 
   # Defines the root path route ("/")
   # root "articles#index"
