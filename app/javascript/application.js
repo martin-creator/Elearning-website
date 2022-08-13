e0f82c4b38536d41b6d4633fd163c64dab850d4c
martin-creator <martinlubowa@outlook.com>
2022-08-13 10:01:34 +0300

    Added materialize css

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,19 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+import "@hotwired/turbo-rails"
+import "controllers"
+//= require materialize
+//= require rails-ujs
+//= require jquery
+//= require materialize-sprockets
+//= require turbolinks
+//= require_tree .
+
+ $(document).ready(function(){
+  $('.modal').modal();
+  $('.sidenav').sidenav();
+  $('.dropdown-trigger').dropdown();
+});
+
