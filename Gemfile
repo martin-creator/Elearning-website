e0103f845332212aa07a967fa8feb56fac3c3b86
martin-creator <martinlubowa@outlook.com>
2022-12-02 14:35:25 +0300

    Created drag and drop feature in the dashboard

--- Gemfile
+++ Gemfile
@@ -66,6 +66,13 @@ gem 'coderay', '~> 1.1', '>= 1.1.3'
 
 gem 'friendly_id', '~> 5.4', '>= 5.4.1'
 
+gem 'active_admin-sortable_tree', '~> 2.1'
+
+gem 'bourbon'
+
+gem 'neat'
+
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: %i[mri mingw x64_mingw]
--- app/admin/tasks.rb
+++ app/admin/tasks.rb
@@ -16,5 +16,23 @@ ActiveAdmin.register Task do
   # end
 
   permit_params :title, :note, :video, :header, :tag, :project_id
+
+  sortable tree: false,
+            sorting_attribute: :tag
   
+  index :as => :sortable do
+    label :title
+
+    actions
+  end
+
+  index do
+    selectable_column
+    column :header
+    column :title
+    column :tag
+    column :project
+
+    actions
+  end
 end
