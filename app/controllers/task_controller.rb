48db441d419f9b10a3aea9ca50cce9ff25dafc5f
martin-creator <martinlubowa@outlook.com>
2022-12-02 18:34:24 +0300

    Updated task controller

--- app/controllers/task_controller.rb
+++ app/controllers/task_controller.rb
@@ -1,11 +1,26 @@
 class TaskController < ApplicationController
-  def show
-    @project = Project.friendly.find(params[:project_id])
-    @tasks = @project.tasks.order(:tag)
-    @task = @tasks.find(params[:id])
+  before_action :authenticate_user!
 
-    @next_task = @task.next
-  	@prev_task = @task.prev
+  def show
+    project = Project.friendly.find(params[:project_id])
+    @tasks = project.tasks.order(:tag)
     
+
+    joined = false
+
+  	if !current_user.nil? && !current_user.projects.nil?
+  		joined = current_user.projects.include?(@project)
+  	end
+
+    if joined
+
+      @task = @tasks.find(params[:id])
+      @next_task = @task.next
+      @prev_task = @task.prev
+    else 
+     flash[:notice] = "No permission to this!"
+      redirect_to project
+    end
+
   end
 end
